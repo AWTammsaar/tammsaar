21fd6b46c1546ae9347d6dedda0289499b327d38
Al William Tammsaar <a.william.tammsaar@gmail.com>
2017-10-13 00:30:01 +0200

    Optimize pageload

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,22 @@
 var gulp = require('gulp');
 var pug = require('gulp-pug');
 var stylus = require('gulp-stylus');
+var concat = require('gulp-concat');
+var minify = require('gulp-minify-css');
+var Filter = require('gulp-filter');
 
 gulp.task('css', function () {
-    return gulp.src('./src/style.styl')
-        .pipe(stylus({
-            compress: true
-        }))
+    const f = Filter('**/*.styl', { restore: true });
+    return gulp.src([
+        './css/normalize.css',
+        './css/skeleton.css',
+        './src/style.styl'
+        ])
+        .pipe(f)
+        .pipe(stylus())
+        .pipe(f.restore)
+        .pipe(concat('style.min.css'))
+        .pipe(minify())
         .pipe(gulp.dest('./css/'));
 });
 
